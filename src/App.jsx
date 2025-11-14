fb5367744fcf16bc722712a55002b45ad9e0181c
Raushan kumar <raushankr1903@gmail.com>
2025-11-14 10:11:26 +0530

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App(){
+  const wordList=['apple','blood','bro','family','bhaiya']
+  
+
+  return(
+   <main>
+    <div className="details-container">
+      <h1>Assembly: Endgame</h1>
+      <span>Guess the word in under 8 attempts to keep the <br /> programming world safe from Assembly!</span>
+    </div>
+   </main>
+  )
+}
